ce39e7df1837da51ebac2811e82e4b620558bbb2
Deploy Fixer <deploy@fix.com>
2026-03-13 17:26:04 +0530

    fix: add HelmetProvider to fix blog page crash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { HelmetProvider } from 'react-helmet-async';
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -114,19 +115,21 @@ function Router() {
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <LanguageProvider>
-          <ErrorBoundary>
-            <AuthProvider>
-              <TooltipProvider>
-                <Router />
-                <Toaster />
-              </TooltipProvider>
-            </AuthProvider>
-          </ErrorBoundary>
-        </LanguageProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <LanguageProvider>
+            <ErrorBoundary>
+              <AuthProvider>
+                <TooltipProvider>
+                  <Router />
+                  <Toaster />
+                </TooltipProvider>
+              </AuthProvider>
+            </ErrorBoundary>
+          </LanguageProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
